59d73aa9a9d44aead1628ae571dbee22914fdb68
Sébastien Graveline <graveline.seb@gmail.com>
2025-10-07 14:00:18 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const myLocalPackage = require("my-local-package");
 
 console.log(myLocalPackage.greet("World"));
+
+const axios = require("axios");
+
+async function fetchData() {
+	try {
+		const response = await axios.get(
+			"https://github.com",
+		);
+		console.log("Fetched data:", response.data);
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
+}
+
+fetchData();
